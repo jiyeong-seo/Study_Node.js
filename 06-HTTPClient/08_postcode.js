561e338d2a6fc1138a6d813eb18a50e9ec67d7b0
sjy <jen.jyseo@gmail.com>
2021-12-19 01:00:44 +0900

    commit

--- 06-HTTPClient/08_postcode.js
+++ 06-HTTPClient/08_postcode.js
@@ -6,10 +6,10 @@ const axios = require("axios");
   try {
     // axios를 활용하여 다른 백엔드에게 HTTP GET 파라미터를 전달하고 결과를 리턴받는다.
     const response = await axios.get(
-      "http://www.juso.go.kr/addrlink/addrLinkApi.do",
+      "https://www.juso.go.kr/addrlink/addrLinkApi.do",
       {
         params: {
-          confmKey: "", // 발급받은 승인키
+          confmKey: "devU01TX0FVVEgyMDIxMTIxNzIzMzQzMjExMjA0NTY=", // 발급받은 승인키
           currentPage: 1, // 현재 페이지 번호
           currentPerPage: 20, // 페이지당 출력할 결과 Row 수
           keyword: "서초W", // 주소 검색어
@@ -24,11 +24,11 @@ const axios = require("axios");
     console.error(errorMessage);
     return;
   }
+  console.log(`json: %o`, json);
 
   json.results.juso.map((item, index) => {
     console.log("[%s] ", item.zipNo);
     console.log("[지번주소]" + item.jibunAddr);
     console.log("[도로명주소]" + item.roadAddr);
-    console.log();
   });
 })();
